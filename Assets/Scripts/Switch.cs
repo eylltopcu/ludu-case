932abf9bdacfae7c0063426a233e837ba6598532
Eylül Topçu <topcueyll@gmail.com>
2026-02-01 15:15:00 +0300

    feature: light switch

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Switch : MonoBehaviour, IInteractable
+{
+    public GameObject objectToToggle; // Drag the object you want to turn on/off
+    public bool isOn = false;
+
+    void Start()
+    {
+        // Make sure object starts in correct state
+        if (objectToToggle != null)
+            objectToToggle.SetActive(isOn);
+        
+        // Disable outline at start
+        Outline outline = GetComponent<Outline>();
+        if (outline != null)
+            outline.enabled = false;
+    }
+
+    public void Interact()
+    {
+        isOn = !isOn;
+        
+        if (objectToToggle != null)
+        {
+            objectToToggle.SetActive(isOn);
+            Debug.Log(isOn ? "Object activated!" : "Object deactivated!");
+        }
+        else
+        {
+            Debug.LogWarning("No object assigned to toggle!");
+        }
+    }
+
+    public bool CanInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+}
